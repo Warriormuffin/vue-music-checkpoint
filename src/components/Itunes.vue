eac56f920e1e529acb188756e46171037b361762
Nathan Elg <elgn@students.wwwu.edu>
2017-05-19 14:19:45 -0600

    we get data to the screen, now to save to our own playlist

--- src/components/Itunes.vue
+++ src/components/Itunes.vue
@@ -1,38 +1,64 @@
 <template>
   <div class="itunes">
+    <div class="search-bar">
       <input type="text" v-model="query">
       <button type='button' @click="search">Search Artist</button>
-  </div>
-
+    </div> <br>
+    <div class="row">
+      <div v-for='artist in this.currentArtistData' class="col-xl-6">
+          <div class="card text-center">
+            <div class="card-block">
+              <img :src='artist.albumUrl30' alt="">
+              <h3>{{artist.artistName}}</h3>
+              <h4 class="card-title">{{artist.trackName}}</h4>
+              <p class="card-text">{{artist.collectionName}}</p>
+              <p class="card-text">{{artist.collectionPrice}}</p>
+              <a @click="addSongToMyPlaylist(artist)" class="btn btn-primary">Add To Playlist</a>
+            </div>
+          </div>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script>
-import ItunesService from '@/services/itunes-service'
-export default {
-  name: 'itunes',
-  data(){
-    return {
-      query: "",
-      currentArtistData: {}
+  import ItunesService from '@/services/itunes-service'
+  import MyTunesService from '@/services/mytunes-service'
+  export default {
+    name: 'itunes',
+    data() {
+      return {
+        query: "",
+        currentArtistData: {}
 
-    }
-  },
-  computed:{},
-  methods:{
-    search(){
-      debugger
-      ItunesService.getMusicByArtist(this.query).then(artistData=>{
-        this.currentArtistData = artistData
-      })
-    }
-  },
-  components:{}
-}
+      }
+    },
+    computed: {},
+    methods: {
+      search() {
+        debugger
+        ItunesService.getMusicByArtist(this.query).then(artistData => {
+          this.currentArtistData = JSON.parse(artistData)
+          this.currentArtistData = this.currentArtistData.results
+          console.log(this.currentArtistData)
+        })
+      },
+      addSongToMyPlaylist(newlyAddedArtist){
+        MyTunesService.addTrack(newlyAddedArtist)
+      }
+    },
+    components: {}
+  }
+
 </script>
 
 
 <style scoped>
-
+  .search-bar {
+    text-align: center;
+  }
+  .card-columns{
+    display: inline-block
+  }
 </style>
